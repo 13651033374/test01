e9f3240ec9d32d584a4959ffd7d4dd119dba4c71
13651033374 <chky2011@163.com>
2019-03-08 11:21:54 +0800

    首次上传

---
+++ scripts/test_login.py
@@ -0,0 +1,16 @@
+import random
+
+
+class TestLogin:
+
+    def test_login1(self):
+        print("1")
+
+    def test_login2(self):
+        print("2")
+
+    def test_login3(self):
+        print("3")
+        assert 0
+
+
